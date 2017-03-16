a7e5e86cd6396bc069c67c522cf5e4fc045d880e
severnsc <severnsc@gmail.com>
2017-03-15 21:16:46 -0400

    Add sessions controller

--- api-testing/front-end/app/controllers/users_controller.rb
+++ api-testing/front-end/app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
 
   def new
   end
+
+  def show
+    @user = 
+  end
 end
--- api-testing/front-end/config/routes.rb
+++ api-testing/front-end/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new]
+  resources :users, only: [:new, :show]
 
   root 'users#new'
 end
